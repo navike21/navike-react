1c73a4b05672403179a7f121f4fff64db5164c8f
navike21 <ivan@navike21.com>
2018-12-08 17:23:35 -0500

    upload data

--- src/components/header.js
+++ src/components/header.js
@@ -5,19 +5,22 @@ import {
   Link
 } from "react-router-dom";
 
+import {Facebook, Instagram, Github, Youtube} from "./export/icons";
+
 import Logo from "../images/logo.svg";
 
-function MenuLink({ label, to, active }) {
+function MenuLink({ label, to, active, showMenuFunction }) {
   return (
-      <Route
-        path={to}
-        exact={active}
-        children={({ match }) => (
-          <li className={"w_55_desktop w_25 align_center " + [match ? "active" : ""]}>
-            <Link to={to}>{label}</Link>
-          </li>
-        )}
-      />
+    <Route
+      path={to}
+      exact={active}
+      showMenuFunction={showMenuFunction}
+      children={({ match }) => (
+        <li className={"w_100 " + [match ? "active" : ""]} onClick={showMenuFunction}>
+          <Link to={to}>{label}</Link>
+        </li>
+      )}
+    />
   );
 }
 
@@ -25,25 +28,58 @@ export default class Header extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      activeMenu: false,
       flag: "esp"
     };
-  }  
+  }
+
+  showMenu = () => {
+    this.setState({
+      activeMenu: !this.state.activeMenu
+    })
+  }
+
+  showMenuLogo = () =>{
+    this.setState({
+      activeMenu: false
+    })
+  }
 
   render() {
     return (
       <header className="section_middle_justify">
-        <Link to="/" className="w_11_desktop img_medium_mobile">
+        <Link to="/" className="w_11_desktop img_medium_mobile logo" onClick={this.showMenuLogo}>
           <img src={Logo} alt="Logo navike" className="w_100" />
         </Link>
-        <button className="icon_menu font_semibold blueColor">
+        <button className="icon_menu font_semibold blueColor" onClick={this.showMenu}>
           <i className="fas fa-bars font_big"></i>
         </button>
-        <nav className="align_right">
+        <nav className={"section_middle_center " + (this.state.activeMenu ? "nav_active" : "nav_inactive")}>
+          <ul className="menu w_80 section_middle_center">
+            <MenuLink active={true} to="/about-me" label="Quién soy?" showMenuFunction={this.showMenu} />
+            <MenuLink to="/services" label="Mis servicios" showMenuFunction={this.showMenu} />
+            <MenuLink to="/job" label="Mi trabajo" showMenuFunction={this.showMenu} />
+            <MenuLink to="/blog" label="Blog" showMenuFunction={this.showMenu} />
+            <MenuLink to="/contact" label="Trabajamos?" showMenuFunction={this.showMenu} />
+          </ul>
+          <aside className="w_80 section_middle_right">
+            <ul className="section_middle_right w_100">
+              <li>
+                <Link to="./" className="rss_icons"><Facebook /></Link>
+              </li>
+              <li>
+                <Link to="./" className="rss_icons"><Instagram /></Link>
+              </li>
+              <li>
+                <Link to="./" className="rss_icons"><Github /></Link>
+              </li>
+              <li>
+                <Link to="./" className="rss_icons"><Youtube /></Link>
+              </li>
+            </ul>
+          </aside>
         </nav>
-        <ul className="menu section_middle_center">
-          <MenuLink active={true} to="/" label="Home" />
-          <MenuLink to="/about" label="About" />
-        </ul>
+
       </header>
     )
   }
